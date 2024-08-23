c5251b03f08ef04454a13ebac9b99bd973aca861
Rocky Warren <rocky.warren@clipboardhealth.com>
2024-08-22 21:10:35 -0600

    refactor: for to forEach

--- packages/json-api/src/lib/toSearchParams.ts
+++ packages/json-api/src/lib/toSearchParams.ts
@@ -11,9 +11,9 @@ export function toSearchParams(query: JsonApiQuery): URLSearchParams {
   const searchParams = new URLSearchParams();
 
   if (query.fields) {
-    for (const [type, fields] of Object.entries(query.fields)) {
+    Object.entries(query.fields).forEach(([type, fields]) => {
       searchParams.append(`fields[${type}]`, fields.join(","));
-    }
+    });
   }
 
   handleFilter(query, searchParams);
@@ -23,9 +23,9 @@ export function toSearchParams(query: JsonApiQuery): URLSearchParams {
   }
 
   if (query.page) {
-    for (const [key, value] of Object.entries(query.page)) {
+    Object.entries(query.page).forEach(([key, value]) => {
       searchParams.append(`page[${key}]`, value);
-    }
+    });
   }
 
   if (query.sort) {
@@ -37,14 +37,14 @@ export function toSearchParams(query: JsonApiQuery): URLSearchParams {
 
 function handleFilter(query: JsonApiQuery, searchParams: URLSearchParams) {
   if (query.filter) {
-    for (const [key, value] of Object.entries(query.filter)) {
+    Object.entries(query.filter).forEach(([key, value]) => {
       if (Array.isArray(value)) {
         searchParams.append(`filter[${key}]`, value.join(","));
       } else if (typeof value === "object") {
-        for (const [subKey, subValue] of Object.entries(value)) {
+        Object.entries(value).forEach(([subKey, subValue]) => {
           searchParams.append(`filter[${key}][${subKey}]`, String(subValue));
-        }
+        });
       }
-    }
+    });
   }
 }
