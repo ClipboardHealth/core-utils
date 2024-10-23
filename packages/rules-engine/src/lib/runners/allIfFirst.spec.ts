917c9fc697a809ce4dfa037b2623b51123a7d593
Yanko Valera <yanko.valera@clipboardhealth.com>
2024-10-23 12:41:46 +0200

    feat(PRI-1321): Create a new runner in core-utils (#18)
    Changes
    ---
    - We need a new runner that allows us to chain rules that will run
    **only** when the first rule runIf = true.
    - This will help us to chain rules like the fixedRatesWorkplaces
    [here](https://github.com/ClipboardHealth/open-shifts/blob/main/curated-shifts/src/modules/shift-offers/pricing-engine/rules/fixed-rates-workplaces.ts#L30),
    instead of manually calling a rule inside another.
    ```typescript
    const fixedRatesWorkplaces = allIfFirst(fixedBasePay, holidayPay, manualCharge)
    ``` 
    And this will run only the fixedBasePay runIf = true (each subsequent
    rule will verify it's runIf as well).
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced the `allIfFirst` function to execute rules based on the
    success of the first rule.
    - Added a suite of unit tests for the `allIfFirst` function to ensure
    expected behavior under various conditions.
    
    - **Bug Fixes**
    - Improved rule execution flow by ensuring subsequent rules are only
    processed if the first rule's condition is met.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

---
+++ packages/rules-engine/src/lib/runners/allIfFirst.spec.ts
@@ -0,0 +1,68 @@
+import { type Rule, type RuleContext } from "../..";
+import { appendOutput } from "../appendOutput";
+import { allIfFirst } from "./allIfFirst";
+
+interface Input {
+  a: number;
+  b: number;
+}
+
+type Output = number;
+
+type TestContext = RuleContext<Input, Output>;
+
+type TestRule = Rule<Input, Output>;
+
+const context: TestContext = {
+  input: { a: 1, b: 2 },
+  output: [],
+};
+
+const testRule1: TestRule = {
+  runIf: () => false,
+  run: (context) => appendOutput(context, 1),
+};
+
+const testRule2: TestRule = {
+  runIf: () => true,
+  run: (context) => appendOutput(context, 2),
+};
+
+const testRule3: TestRule = {
+  runIf: () => true,
+  run: (context) => appendOutput(context, 3),
+};
+
+const testRule4: TestRule = {
+  runIf: () => false,
+  run: (context) => appendOutput(context, 4),
+};
+
+describe("allIfFirst", () => {
+  describe("if", () => {
+    it("returns true if the first rule is true", () => {
+      expect(allIfFirst(testRule2, testRule1).runIf(context.input)).toBe(true);
+    });
+
+    it("returns false if the first rule is false", () => {
+      expect(allIfFirst(testRule1, testRule2).runIf(context.input)).toBe(false);
+    });
+
+    it("returns false if the array is empty", () => {
+      expect(allIfFirst().runIf(context.input)).toBe(false);
+    });
+  });
+
+  describe("run", () => {
+    it("runs all the matching rules", () => {
+      expect(allIfFirst(testRule2, testRule1, testRule3, testRule4).run(context)).toEqual({
+        ...context,
+        output: [2, 3],
+      });
+    });
+
+    it("returns the received context if no rule can be run", () => {
+      expect(allIfFirst(testRule1, testRule4).run(context)).toEqual(context);
+    });
+  });
+});
