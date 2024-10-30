7d346f1aa7df22bbf945528d1ccf112ae3f98495
Rodrigo Sasaki <rodrigo.sasaki@clipboardhealth.com>
2024-10-30 00:41:08 -0300

    feat: removing knownmetadata interface

--- packages/execution-context/src/types/types.ts
+++ packages/execution-context/src/types/types.ts
@@ -1,35 +1,4 @@
-/**
- * This interface contains the common attributes that we often want to know about
- * in any execution within our domain.
- */
-interface KnownMetadata {
-  /**
-   * the id of the shift being referenced in the execution
-   */
-  shiftId?: string;
-
-  /**
-   * @deprecated use `workerId` instead.
-   */
-  agentId?: string;
-
-  /**
-   * the id of the worker being referenced in the execution
-   */
-  workerId?: string;
-
-  /**
-   * @deprecated use `workplaceId` instead.
-   */
-  facilityId?: string;
-
-  /**
-   * the id of the workplace being referenced in the execution
-   */
-  workplaceId?: string;
-}
-
-export type Metadata = KnownMetadata & Record<string, unknown>;
+export type Metadata = Record<string, unknown>;
 
 export interface ExecutionContext {
   /**
