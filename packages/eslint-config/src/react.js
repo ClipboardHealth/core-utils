28eca7a2e47bfc1501417d9c45636090c31b1447
Dylan <dylanwebb@me.com>
2025-10-13 18:26:57 -0400

    feat(INV-2258): Remove react/prefer-read-only-props from eslint config (#222)
    Summary
    ===
    
    - This rule causes `readonly` to be added to every single prop, which
    adds bloat to the react apps, but provides no return since it prevents
    reassigning which is already handled by another lint rule.

--- packages/eslint-config/src/react.js
+++ packages/eslint-config/src/react.js
@@ -1,5 +1,9 @@
 module.exports = {
   extends: ["./index", "xo-react/space"],
+  rules: {
+    // Adds bloat, and is redundant with no-param-reassign
+    "react/prefer-read-only-props": "off",
+  },
   parserOptions: {
     project: ["tsconfig.json"],
     tsconfigRootDir: __dirname,
