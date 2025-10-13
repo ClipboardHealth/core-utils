efa540e6b00f1513d3697942b1ce42d605e5b876
Dylan Webb <dylan.webb@clipboardhealth.com>
2025-10-13 10:41:22 +0900

    feat(INV-2258): Remove react/prefer-read-only-props from eslint config

--- packages/eslint-config/src/react.spec.ts
+++ packages/eslint-config/src/react.spec.ts
@@ -4,6 +4,9 @@ describe("eslint-config", () => {
   it("matches", () => {
     expect(config).toEqual({
       extends: ["./index", "xo-react/space"],
+      rules: {
+        "react/prefer-read-only-props": "off",
+      },
       parserOptions: {
         project: ["tsconfig.json"],
         tsconfigRootDir: __dirname,
