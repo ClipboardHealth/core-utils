2c47fb358bedbf5a4de7fc26e87f5bd030e6e6ea
Dylan Webb <dylan.webb@clipboardhealth.com>
2025-10-14 14:56:08 +0900

    feat(INV-2297): Allow objectId coercion

--- packages/contract-core/src/lib/schemas/objectId.ts
+++ packages/contract-core/src/lib/schemas/objectId.ts
@@ -3,4 +3,4 @@ import { z } from "zod";
 /**
  * A MongoDB ObjectId string.
  */
-export const objectId = z.string().regex(/^[\dA-Fa-f]{24}$/, "Must be a valid ObjectId");
+export const objectId = z.coerce.string().regex(/^[\dA-Fa-f]{24}$/, "Must be a valid ObjectId");
